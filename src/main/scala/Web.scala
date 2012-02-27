8b09905e57e403a53c7b0e2d7bbe68e137c6577e
Jon-Anders Teigen <mail@jteigen.com>
2012-02-27 19:35:03 +0100

    fixs

--- src/main/scala/Web.scala
+++ src/main/scala/Web.scala
@@ -8,9 +8,8 @@ object Web {
   def main(args: Array[String]) {
     val port = Properties.envOrElse("PORT", "8080").toInt
     println("Starting on port:" + port)
-    jetty.Http(port).resources(getClass().getResource("/bootstrap")).resources(getClass().getResource("/html")).filter(filter.Planify {
+    jetty.Http(port).resources(getClass().getResource("/static")).filter(filter.Planify {
       case GET(Path("/foo")) => Html(<h1>bar</h1>)
-      case GET(_) => Html(<h1>hello</h1>)
     }).run
   }
 }
