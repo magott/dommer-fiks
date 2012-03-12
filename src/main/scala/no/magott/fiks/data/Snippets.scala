9b4e6baa82365272abf850cc05403f5fb02e0c55
Morten Andersen-Gott <morten.andersengott@gmail.com>
2012-03-12 21:50:56 +0100

    Favicon and branding. Closes #1

--- src/main/scala/no/magott/fiks/data/Snippets.scala
+++ src/main/scala/no/magott/fiks/data/Snippets.scala
@@ -26,6 +26,7 @@ object Snippets {
         """}
         </style>
           <link href="/css/bootstrap-responsive.css" rel="stylesheet"/>
+          <link href="/css/fiks.css" rel="stylesheet"/>
 
         <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
         <!--[if lt IE 9]>
