549eaa06c189ded46e0e95a6db400bac5f9d68aa
Morten Andersen-Gott <morten.andersengott@gmail.com>
2012-03-12 09:46:41 +0100

    Extractor for fiks cookie

--- src/main/scala/no/magott/fiks/data/FiksCookie.scala
+++ src/main/scala/no/magott/fiks/data/FiksCookie.scala
@@ -1,11 +1,14 @@
 package no.magott.fiks.data
 
-import unfiltered.request.HttpRequest
+import unfiltered.request.{Cookies, HttpRequest}
 
 object FiksCookie {
-
-//  def unapply[T](req: HttpRequest[T]) = {
-////    req.
-//  }
+  def unapply[T](req: HttpRequest[T]) = {
+    val cookies = Cookies.unapply(req).get
+    cookies("fiksToken") match {
+      case Some(c) => Some(c.value)
+      case None => None
+    }
+  }
 
 }
