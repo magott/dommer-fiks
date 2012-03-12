d2653f676f4d0f8fed1bbacdb331a9ff9d7ad72d
Morten Andersen-Gott <morten.andersengott@gmail.com>
2012-03-12 23:19:37 +0100

    Added google analytics

--- src/main/scala/no/magott/fiks/data/Snippets.scala
+++ src/main/scala/no/magott/fiks/data/Snippets.scala
@@ -70,13 +70,26 @@ object Snippets {
         <div class="alert alert-info">Denne siden er under utvikling og kan derfor være noe ustabil mens mer funksjonalitet utvikles. </div>
         <footer>
           <p><a href="http:///www.andersen-gott.com">Morten Andersen-Gott</a> (c) 2012</p>
-          <p><a href="https://github.com/magott/ofk-fiks/issues">Foreslå forbedringer eller rapportér feil</a></p>
+          <p><a href="http://www.facebook.com/dommerfiks">Foreslå forbedringer eller rapportér feil</a></p>
         </footer>
 
 
         <!-- Placed at the end of the document so the pages load faster -->
         <script src="/js/jquery.js"></script>
         <script src="/js/bootstrap.js"></script>
+        <script type="text/javascript">
+          {"""
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-29904717-1']);
+          _gaq.push(['_trackPageview']);
+          (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+  """}
+
+  </script>
       </body>
     </html>
 
