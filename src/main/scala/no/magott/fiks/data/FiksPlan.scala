29d74a9f443cab56aefacb4203e38f62f9b7d6c8
Morten Andersen-Gott <morten.andersengott@gmail.com>
2012-03-12 19:34:44 +0100

    Mapping root to my matches

--- src/main/scala/no/magott/fiks/data/FiksPlan.scala
+++ src/main/scala/no/magott/fiks/data/FiksPlan.scala
@@ -2,31 +2,22 @@ package no.magott.fiks.data
 
 import unfiltered.filter.Plan
 import unfiltered.request._
-import unfiltered.Cookie
 import unfiltered.response._
-import org.joda.time.LocalDateTime
 
-object FiksPlan extends Plan{
+object FiksPlan extends Plan {
   def intent = {
-    case GET(Path(Seg("fiks"::"mymatches" :: Nil))) & Cookies(cookies)=>
-      cookies("fiksToken") match{
-        case Some(Cookie(_, loginToken, _, _, _, _, _, _)) =>
-          redirectToLoginIfTimeout{
-            Ok ~> Html({
-            Pages.assignedMatches(MatchScraper.assignedMatches(FiksLogin.COOKIE_NAME, loginToken))
-          })
-          }
-        case _ => Redirect("/login?message=loginRequired")
-      }
-    case GET(Path(Seg("ical"::Nil))) & Params(p) => Ok ~> CalendarContentType ~> ResponseString(Snippets.isc(p))
-    case GET(Path(Seg("/"::Nil))) => Redirect("/fiks/mymatches")
+    case GET(Path(Seg("fiks" :: "mymatches" :: Nil))) & FiksCookie(loginToken) => redirectToLoginIfTimeout {
+      Ok ~> Html(Pages.assignedMatches(MatchScraper.assignedMatches(FiksLogin.COOKIE_NAME, loginToken)))
+    }
+    case GET(Path(Seg("ical" :: Nil))) & Params(p) => Ok ~> CalendarContentType ~> ResponseString(Snippets.isc(p))
+    case GET(Path(Seg(Nil))) => Redirect("/fiks/mymatches")
   }
 
   def redirectToLoginIfTimeout(f: => ResponseFunction[Any]) = {
-    try{
+    try {
       f
-    }catch {
-      case e:SessionTimeoutException => Redirect("/login?message=sessionTimeout")
+    } catch {
+      case e: SessionTimeoutException => Redirect("/login?message=sessionTimeout")
     }
   }
 
--- src/main/scala/no/magott/fiks/data/Pages.scala
+++ src/main/scala/no/magott/fiks/data/Pages.scala
@@ -25,16 +25,15 @@ object Pages {
   }
 
   def assignedMatches(assignedMatches: Iterator[AssignedMatch]) = {
-    {
       emptyPage(tableOfAssignedMatches(assignedMatches))
-    }
   }
 
   def about = {
     <h2>Om denne siden</h2>
       <p>
         Denne siden er utviklet av Morten Andersen-Gott. Dersom du har innspill til forbedringer eller vil rapportere
-        en feil kan du gjøre dette <a href="https://github.com/magott/ofk-fiks/issues">her</a>
+        en feil kan du gjøre dette
+        <a href="https://github.com/magott/ofk-fiks/issues">her</a>
       </p>
 
       <h2>Sikkerhet</h2>
@@ -43,10 +42,18 @@ object Pages {
         I alphaversjon sendes brukernavn og passord i klartekst. Dette betyr at det er enkelt for kyndige personer å lese
         passord og brukernavnet ditt. I fremtiden vil passord og brukernavn gå over HTTPS slik at dette ikke er noe problem.
       </p>
-    <p>
-      Dommer-FIKS ser ditt brukernavn og passord, men hverken lagrer ikke dette.
-      Det finnes ingen steder i Dommer-FIKS etter innlogging er fortetatt
-    </p>
+      <p>
+        Dommer-FIKS ser ditt brukernavn og passord, men hverken lagrer ikke dette.
+        Det finnes ingen steder i Dommer-FIKS etter innlogging er fortetatt
+      </p>
+
+      <h2>Donér</h2>
+      <p>
+        Jeg har ingen direkte utgifter (foreløpig) forbundet med utvikling og drift av Dommer-FIKS, men jeg bruker en del tid
+        på det. Har du lyst til å vise at du setter pris på tjenesten? Jeg setter stor pris på bidrag via paypal
+
+
+      </p>
 
   }
 
