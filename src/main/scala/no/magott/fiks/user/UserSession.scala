730720ed1e303144be3cb12fe9230c4749852ad2
Morten Andersen-Gott <morten.andersengott@gmail.com>
2012-05-05 00:12:16 +0200

    Adding created date to session

--- src/main/scala/no/magott/fiks/user/UserSession.scala
+++ src/main/scala/no/magott/fiks/user/UserSession.scala
@@ -1,13 +1,19 @@
 package no.magott.fiks.user
 
 import com.mongodb.casbah.commons.MongoDBObject
+import org.joda.time.DateTime
 
 case class UserSession(fiksToken:String, username:String) {
 
+  private val created = DateTime.now();
+
   def asMongoDbObject = {
+    import com.mongodb.casbah.commons.conversions.scala._
+    RegisterJodaTimeConversionHelpers()
     val builder = MongoDBObject.newBuilder
     builder += "fiksToken" -> fiksToken
     builder += "username" -> username
+    builder += "created" -> created
     builder.result
   }
 
