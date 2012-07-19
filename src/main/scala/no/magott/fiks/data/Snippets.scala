f58c1e53102264784a1e0ddca0550002671debeb
Morten Andersen-Gott <morten.andersengott@gmail.com>
2012-07-19 17:20:00 +0200

    Fix nav bar bug to allow navigation with Opera Mini

--- src/main/scala/no/magott/fiks/data/Snippets.scala
+++ src/main/scala/no/magott/fiks/data/Snippets.scala
@@ -18,11 +18,11 @@ case class Snippets[T <: HttpServletRequest] (req: HttpRequest[T]) {
     <div class="navbar navbar-fixed-top">
       <div class="navbar-inner">
         <div class="container">
-          <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
-          </a>
+          </button>
           <a class="brand" href="/">Dommer-FIKS</a>
           <div class="nav-collapse">
             <ul class="nav">
