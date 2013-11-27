b75132051e5e2db710e340c7d3bc7c1d4cd64bd1
Morten Andersen-Gott <morten.andersengott@gmail.com>
2013-11-27 22:07:18 +0100

    Fix login problems after loginform lost a hidden field

--- src/main/scala/no/magott/fiks/data/FiksLoginService.scala
+++ src/main/scala/no/magott/fiks/data/FiksLoginService.scala
@@ -9,36 +9,17 @@ object FiksLoginService {
 
   val COOKIE_NAME = "ASP.NET_SessionId"
 
-  def doLogin(username: String, password: String) = {
-    val loginPage = Jsoup.connect("https://fiks.fotball.no/Fogisdomarklient/Login/Login.aspx").method(Method.GET).timeout(10000).execute()
-    val loginDocument = loginPage.parse
-    val sessionId = loginPage.cookie(COOKIE_NAME)
-    val viewstate = loginDocument.getElementById("__VIEWSTATE").attr("value")
-    val eventvalidation = loginDocument.getElementById("__EVENTVALIDATION").attr("value")
-    val params = Map("tbAnvandarnamn" -> username, "tbLosenord" -> password, "__VIEWSTATE" -> viewstate, "__EVENTVALIDATION" -> eventvalidation, "btnLoggaIn" -> "Logg inn")
-    val response = Jsoup.connect("https://fiks.fotball.no/Fogisdomarklient/Login/Login.aspx")
-      .data(params.asJava)
-      .method(Method.POST)
-      .cookie(COOKIE_NAME, sessionId)
-      .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11")
-      .followRedirects(false)
-      .timeout(150000)
-      .execute()
-
-    if (response.statusCode == 302) {
-      Right((COOKIE_NAME, sessionId))
-    } else {
-      Html5(<h1>Feil</h1>)
-    }
-  }
 
   def login(username: String, password: String) = {
     val loginPage = Jsoup.connect("https://fiks.fotball.no/Fogisdomarklient/Login/Login.aspx").method(Method.GET).timeout(10000).execute()
     val loginDocument = loginPage.parse
     val sessionId = loginPage.cookie(COOKIE_NAME)
-    val viewstate = loginDocument.getElementById("__VIEWSTATE").attr("value")
-    val eventvalidation = loginDocument.getElementById("__EVENTVALIDATION").attr("value")
-    val params = Map("tbAnvandarnamn" -> username, "tbLosenord" -> password, "__VIEWSTATE" -> viewstate, "__EVENTVALIDATION" -> eventvalidation, "btnLoggaIn" -> "Logg inn")
+    val viewstate = Option(loginDocument.getElementById("__VIEWSTATE")).flatMap(el => Option(el.attr("value")))
+    val eventvalidation = Option(loginDocument.getElementById("__EVENTVALIDATION")).flatMap(el=> Option(el.attr("value")))
+    val params = Map("tbAnvandarnamn" -> Some(username), "tbLosenord" -> Some(password), "__VIEWSTATE" -> viewstate, "__EVENTVALIDATION" -> eventvalidation, "btnLoggaIn" -> Some("Logg inn"))
+      .collect{
+      case (k, Some(v:String)) => k -> v
+    }
     val response = Jsoup.connect("https://fiks.fotball.no/Fogisdomarklient/Login/Login.aspx")
       .data(params.asJava)
       .method(Method.POST)
