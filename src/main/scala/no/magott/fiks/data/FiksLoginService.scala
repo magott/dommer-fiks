5212ad914df676a31076f740ff638c0caee5740d
Morten Andersen-Gott <morten.andersengott@gmail.com>
2014-03-14 14:25:04 +0100

    Changed login to work with new login from NFF

--- src/main/scala/no/magott/fiks/data/FiksLoginService.scala
+++ src/main/scala/no/magott/fiks/data/FiksLoginService.scala
@@ -4,38 +4,46 @@ import org.jsoup.Jsoup
 import org.jsoup.Connection.Method
 import scala.collection.JavaConverters._
 import unfiltered.response.Html5
+import no.magott.fiks.user.{UserSession, UserService}
 
 object FiksLoginService {
 
-  val COOKIE_NAME = "ASP.NET_SessionId"
 
+  val APP_COOKIE_NAME = ".AspNet.ApplicationCookie"
+  val SESS_COOKIE_NAME = "ASP.NET_SessionId"
+  val LOGIN_FORM_URL = "http://www.fiks.fotball.no/FiksWeb/Login"
+  val LOGIN_URL = "http://www.fiks.fotball.no/FiksWeb/Login?ReturnUrl=~%2FHome%2FConsolidateUsers"
+  val REF_CLIENT_URL = "http://www.fiks.fotball.no/FiksWeb/Home/RedirectToFiksReferee?clubId=0"
+  val VALIDATION_COOKIE_NAME = "__RequestVerificationToken_L0Zpa3NXZWI1"
+  val REQ_VAL_FORMFIELD_NAME = "__RequestVerificationToken"
 
   def login(username: String, password: String) = {
-    val loginPage = Jsoup.connect("https://fiks.fotball.no/Fogisdomarklient/Login/Login.aspx").method(Method.GET).timeout(10000).execute()
+    val loginPage = Jsoup.connect(LOGIN_FORM_URL).method(Method.GET).timeout(10000).execute()
     val loginDocument = loginPage.parse
-    val sessionId = loginPage.cookie(COOKIE_NAME)
-    val viewstate = Option(loginDocument.getElementById("__VIEWSTATE")).flatMap(el => Option(el.attr("value")))
-    val eventvalidation = Option(loginDocument.getElementById("__EVENTVALIDATION")).flatMap(el=> Option(el.attr("value")))
-    val params = Map("tbAnvandarnamn" -> Some(username), "tbLosenord" -> Some(password), "__VIEWSTATE" -> viewstate, "__EVENTVALIDATION" -> eventvalidation, "btnLoggaIn" -> Some("Logg inn"))
+    loginPage.cookies().asScala.foreach(println)
+    val requestValidationCookie = loginPage.cookie(VALIDATION_COOKIE_NAME)
+    val foo = Option(loginDocument.getElementsByAttributeValue("name",REQ_VAL_FORMFIELD_NAME))
+    val requestValidationFormField = foo.flatMap(el=> Option(el.attr("value")))
+    val params = Map("UserName" -> Some(username), "Password" -> Some(password), REQ_VAL_FORMFIELD_NAME -> requestValidationFormField)
       .collect{
       case (k, Some(v)) => k -> v
     }
-    val response = Jsoup.connect("https://fiks.fotball.no/Fogisdomarklient/Login/Login.aspx")
+    val response = Jsoup.connect(LOGIN_URL)
       .data(params.asJava)
       .method(Method.POST)
-      .cookie(COOKIE_NAME, sessionId)
+      .cookie(VALIDATION_COOKIE_NAME, requestValidationCookie)
       .userAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.56 Safari/535.11")
       .followRedirects(false)
       .timeout(15000)
       .execute()
 
     if (response.statusCode == 302) {
-      Right((COOKIE_NAME, sessionId))
+      val applicationCookie = response.cookie(APP_COOKIE_NAME)
+      val sessionCookie = Jsoup.connect(REF_CLIENT_URL).method(Method.GET).timeout(10000).cookie(APP_COOKIE_NAME, applicationCookie).execute().cookie(SESS_COOKIE_NAME)
+      Right(SESS_COOKIE_NAME -> sessionCookie)
     } else {
-      Left(new RuntimeException("Login failed"));
+      Left(new RuntimeException("Login failed"))
     }
   }
-
-
 
 }
