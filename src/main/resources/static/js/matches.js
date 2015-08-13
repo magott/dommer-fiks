708de287635c9494b2050d42345a6a7dca9a073a
Morten Andersen-Gott <morten.andersengott@gmail.com>
2015-08-13 14:14:05 +0200

    Include matches played same day in upcoming filter

--- src/main/resources/static/js/matches.js
+++ src/main/resources/static/js/matches.js
@@ -26,39 +26,39 @@ app.controller("ctrl", function($scope, $http, $window, $location, $interval) {
     };
     $scope.isSet = function(value) {
         return _.isNull(value) != true;
-    }
+    };
     $scope.dateFilter = function(match) {
-        return moment(match.date).isAfter($scope.fromDate);
-    }
+        return moment(match.date).endOf('day').isAfter($scope.fromDate);
+    };
     $scope.reloadMatches = function() {
         $scope.isTimeout = false;
         $scope.isLoading = true;
         $scope.loadMatches();
-    }
+    };
     $scope.isShowingAllMatches = function() {
         return $scope.fromDate.isBefore($scope.today());
-    }
+    };
 
     $scope.setFromDate = function(from) {
         $location.search("from", from.toISOString())
         $scope.fromDate = from;
-    }
+    };
     $scope.today = function() {
         return moment().startOf('day');
-    }
+    };
 
     $scope.getFromInitValue = function() {
-         from = $location.search().from
-         fromUrl = moment(from)
+         from = $location.search().from;
+         fromUrl = moment(from);
          if(fromUrl.isValid){
             return fromUrl;
         }else{
             return today();
         }
-    }
+    };
     $scope.yearAgo = function() {
         return $scope.today().subtract(1, 'year');
-    }
+    };
     $scope.fromDate = $scope.getFromInitValue();
 
     $scope.isReady = function(){return !$scope.isLoading && !$scope.isTimeout;}
