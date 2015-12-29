670ca7daba49401cb21b72040083a2be1dfb677e
Morten Andersen-Gott <morten.andersengott@gmail.com>
2015-12-29 20:08:46 +0100

    Update Mongosettings to use new authorization scheme

--- src/main/scala/no/magott/fiks/data/MongoSetting.scala
+++ src/main/scala/no/magott/fiks/data/MongoSetting.scala
@@ -1,15 +1,20 @@
 package no.magott.fiks.data
 
-import com.mongodb.casbah.{MongoDB, MongoConnection}
+import com.mongodb.ServerAddress
+import com.mongodb.casbah.{MongoCredential, MongoClient, MongoDB, MongoConnection}
 
 object MongoSetting {
   def unapply(url: Option[String]): Option[MongoDB] = {
     val regex = """mongodb://(\w+):(\w+)@([\w|\.]+):(\d+)/(\w+)""".r
     url match {
       case Some(regex(u, p, host, port, dbName)) =>
-        val db = MongoConnection(host, port.toInt)(dbName)
-        db.authenticate(u,p)
-        Some(db)
+        val credential = MongoCredential.createCredential(u, dbName, p.toCharArray)
+        val address = new ServerAddress(host, port.toInt)
+        val client = MongoClient(address, credential :: Nil)
+        Some(client.getDB(dbName))
+//        val db = MongoConnection(host, port.toInt)(dbName)
+//        db.authenticate(u,p)
+//        Some(db)
       case None =>
         Some(MongoConnection("localhost", 27017)("test"))
     }
